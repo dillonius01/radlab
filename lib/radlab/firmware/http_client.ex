8231e02852d9ddc66824a60cc1d930dc1c0f8aab
Dillon Powers <dillon.c.powers@gmail.com>
2019-09-23 16:54:40 -0400

    clean up http_client

--- lib/radlab/firmware/http_client.ex
+++ lib/radlab/firmware/http_client.ex
@@ -1,68 +1,99 @@
 defmodule Radlab.Firmware.HttpClient do
   alias Radlab.Firmware.Client
+  require Logger
   @behaviour Client
 
   @default_headers ['connection: keep-alive', 'keep-alive: timeout=5, max=5000']
 
   @impl Client
+  @doc """
+  Returns a tuple of :ok and the integer checksum from the endpoint configured in `config.exs`
+  If there is an error, returns `{:error, {atom(), details}} `
+  """
   def get_checksum() do
     endpoint = get_endpoint()
 
-    {:ok, {{'HTTP/1.1', status_code, _status}, _headers, body}} =
-      :httpc.request(
-        :post,
-        {endpoint, @default_headers, 'application/text', 'CHECKSUM'},
-        [],
-        []
-      )
-
-    handle_checksum_response(status_code, body)
+    :httpc.request(
+      :post,
+      {endpoint, @default_headers, 'application/text', 'CHECKSUM'},
+      [],
+      []
+    )
+    |> handle_checksum_response()
   end
 
-  defp handle_checksum_response(200, 'Checksum: 0x' ++ checksum) do
+  defp handle_checksum_response({:ok, {{'HTTP/1.1', status_code, _status}, _headers, body}}) do
+    parse_checksum(status_code, body)
+  end
+
+  defp handle_checksum_response({:error, details}) do
+    Logger.error("Encountered httpc error: #{inspect(details)}")
+    {:error, {:httpc_error, details}}
+  end
+
+  defp parse_checksum(200 = status_code, 'Checksum: 0x' ++ checksum = body) do
     trimmed =
       checksum
       |> to_string
       |> String.trim()
 
-    {checksum_int, _} = Integer.parse(trimmed, 16)
+    case Integer.parse(trimmed, 16) do
+      {checksum_int, ""} ->
+        {:ok, checksum_int}
 
-    {:ok, checksum_int}
+      _ ->
+        response_details = create_map_from_httpc_response(status_code, body)
+        {:error, {:malformed_checksum, response_details}}
+    end
   end
 
-  defp handle_checksum_response(status_code, body) do
+  defp parse_checksum(status_code, body) do
     response_details = create_map_from_httpc_response(status_code, body)
     {:error, {:other, response_details}}
   end
 
   @impl Client
+  @doc """
+  Returns `{:ok, :received}` if the chunk was uploaded successfully
+  Returns `{:error, {:error_processing_contents, details}}` if a 200 was received but the
+  body indicated that the chunk was not processed
+
+  For other cases, returns `{:error, {atom(), details}}`
+  """
   def send_chunk(chunk) do
     endpoint = get_endpoint()
     chunk_as_charlist = String.to_charlist(chunk)
 
-    {:ok, {{'HTTP/1.1', status_code, _status}, _headers, body}} =
-      :httpc.request(
-        :post,
-        {endpoint, @default_headers, 'application/x-binary',
-         'CHUNK: ' ++
-           chunk_as_charlist},
-        [],
-        []
-      )
-
-    handle_chunk_response(status_code, body)
+    :httpc.request(
+      :post,
+      {endpoint, @default_headers, 'application/x-binary',
+       'CHUNK: ' ++
+         chunk_as_charlist},
+      [],
+      []
+    )
+    |> handle_chunk_response()
   end
 
-  defp handle_chunk_response(200 = status_code, 'ERROR PROCESSING CONTENTS\n' = body) do
+  defp handle_chunk_response({:ok, {{'HTTP/1.1', status_code, _status}, _headers, body}}) do
+    parse_chunk_success(status_code, body)
+  end
+
+  defp handle_chunk_response({:error, details}) do
+    Logger.error("Encountered httpc error: #{inspect(details)}")
+    {:error, {:httpc_error, details}}
+  end
+
+  defp parse_chunk_success(200 = status_code, 'ERROR PROCESSING CONTENTS\n' = body) do
     response_details = create_map_from_httpc_response(status_code, body)
     {:error, {:error_processing_contents, response_details}}
   end
 
-  defp handle_chunk_response(200, 'OK\n') do
+  defp parse_chunk_success(200, 'OK\n') do
     {:ok, :received}
   end
 
-  defp handle_chunk_response(status_code, body) do
+  defp parse_chunk_success(status_code, body) do
     response_details = create_map_from_httpc_response(status_code, body)
     {:error, {:other, response_details}}
   end
