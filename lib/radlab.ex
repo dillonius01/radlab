8a44464f5dc0049f031cd87418840cf105b43a19
Dillon Powers <dillon.c.powers@gmail.com>
2019-09-23 16:31:42 -0400

    move runner logic into radlab module

--- lib/radlab.ex
+++ lib/radlab.ex
@@ -1,18 +1,33 @@
 defmodule Radlab do
-  @moduledoc """
-  Documentation for Radlab.
-  """
+  require Logger
+  alias Radlab.Firmware.Updater
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Radlab.hello()
-      :world
-
+  Receives options map and base64 encoded_firmware
+  Runs the appropriate tasks based on the given options
   """
-  def hello do
-    :world
+  def run_program(%{upload: false, checksum: true}, encoded_firmware) do
+    run_and_log_checksum(encoded_firmware)
+  end
+
+  def run_program(%{upload: true, checksum: true}, encoded_firmware) do
+    run_and_log_updater(encoded_firmware)
+    run_and_log_checksum(encoded_firmware)
+  end
+
+  def run_program(%{upload: true, checksum: false}, encoded_firmware) do
+    run_and_log_updater(encoded_firmware)
+  end
+
+  defp run_and_log_checksum(encoded_firmware) do
+    checksum_res = Updater.confirm_update_success(encoded_firmware)
+    Logger.info("Checksum response: #{checksum_res}")
+    checksum_res
+  end
+
+  defp run_and_log_updater(encoded_firmware) do
+    update_res = Updater.update_firmware(encoded_firmware)
+    Logger.info("Update response: #{update_res}")
+    update_res
   end
 end
--- test/radlab_test.exs
+++ test/radlab_test.exs
@@ -1,8 +1,7 @@
 defmodule RadlabTest do
   use ExUnit.Case
-  doctest Radlab
 
   test "greets the world" do
-    assert Radlab.hello() == :world
+    # assert Radlab.hello() == :world
   end
 end
